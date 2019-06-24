cff0c0c55e5e47fa627d5ef16031a1fd07a2c9f1
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-06-24 13:21:09 -0500

    pseudo coded the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+//make an array containing all 26 letters
+
+
+//have the computer randomly generate a number then pick a letter from the array
+
+
+// onkeyup store as playerguess
+
+
+//give the player 10 guesses
+
+
+//if guess is correct increase win counter by 1
+
+
+//if guess is wrong decrease counter by 1
+
+
+//if guesses hit 0 then increase losses counter by 1
+
+
+//add letter to the guess section maybe write as a function?
+
+
+//reset the game maybe write as a function?
+
+
